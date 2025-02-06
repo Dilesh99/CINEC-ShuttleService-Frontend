324c633393b443e14cad18ce9cdca49dc7ea8a75
NaveenSandaru <naveensandaru2@gmail.com>
2025-02-06 08:04:39 +0530

    06.02.2025

--- src/backend/authMethods.js
+++ src/backend/authMethods.js
@@ -1,12 +1,48 @@
 import backEndURL from "./backEndApi";
 
 export const authMethods = {
-    refreshToken: async function (role) {
-        const response = await fetch(`${backEndURL}/refresh${role}Token`, {
-            method: "POST"
-        });
-        const data = await response.json();
-        console.log(data);
-        return data.accessToken;
-    }
+    refreshToken: async function () {
+        try {
+            const response = await loopRefreshToken();
+
+            if (response && response.ok) {
+                try {
+                    const data = await response.json();
+                    return {
+                        accessToken: data.accessToken,
+                        ID: data.id,
+                        name: data.name,
+                        role: data.role
+                    };
+                } catch {
+                    return null;
+                }
+            }
+            return null;
+        } catch {
+            return null;
+        }
+    },
 };
+
+async function loopRefreshToken() {
+    const roles = ["Student", "Staff", "Admin", "Driver"];
+
+    for (let role of roles) {
+        try {
+            const response = await fetch(`${backEndURL}/refresh${role}Token`, {
+                method: "POST",
+                credentials: 'include'
+            });
+
+            if (response.status === 401 || response.status === 403) {
+                continue;
+            }
+
+            if (response.ok) return response;
+        } catch {
+            continue;
+        }
+    }
+    return null;
+}
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { CssBaseline, Box, Button, Grid2, TextField, Typography, InputAdornment ,IconButton, CircularProgress, Checkbox } from '@mui/material'
+import React, { useEffect, useState, useRef } from 'react'
+import { CssBaseline, Box, Button, Grid2, TextField, Typography, InputAdornment, IconButton, CircularProgress, Checkbox } from '@mui/material'
 import BG from "../assets/bg5.jpg"
 import IM1 from "../assets/IM1.png"
 import L1 from "../assets/Logo2.png"
@@ -10,14 +10,25 @@ import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 import backEndURL from '../backend/backEndApi'
+import {authMethods} from '../backend/authMethods'
 
 
 
 
 const SignIn = () => {
 
+    const hasRun = useRef(false);
     var response = null;
     useEffect(() => {
+        if (!hasRun.current) {
+            hasRun.current = true;
+            try {
+                handleAutoSignIn();
+            } catch {
+                return null;
+            }
+        }
+    
         document.body.style.overflow = 'hidden';
         return () => {
             document.body.style.overflow = '';
@@ -35,17 +46,46 @@ const SignIn = () => {
 
     let person;
 
-    const handleResetPassword = async () =>{
+    const handleAutoSignIn = async () => {
+        try {
+            const res = await authMethods.refreshToken();
+        if (res) {
+            switch (res.role){
+                case "Student":
+                    window.location.href = "/home"
+                    break;
+                case "Staff":
+                    window.location.href = "/home"
+                    break;
+                case "Admin":
+                    window.location.href = "/adminDashboard"
+                    break;
+                case "Driver":
+                    window.location.href = `/shuttleService/${res.ID}`;
+                    break;
+                default:
+                    break;
+            }
+        }
+        else{
+            return null;
+        }
+        } catch{
+            return null;
+        }
+    }
+
+    const handleResetPassword = async () => {
         selectPerson();
         console.log(person);
         setIsResettingPassword(true);
         setError('');
-        if(ID == ""){
+        if (ID == "") {
             setIsResettingPassword(false);
             return setError('Please Enter ID');
         }
-        if(person == "Student"){
-            try{
+        if (person == "Student") {
+            try {
                 const response = await fetch(`${backEndURL}/studentResetEmail`, {
                     method: "POST",
                     credentials: "include",
@@ -55,20 +95,20 @@ const SignIn = () => {
                     body: JSON.stringify({ studentID: ID, password: "" })
                 });
                 const data = await response.json();
-                if(!data.success){
+                if (!data.success) {
                     throw new Error("Student not found");
                 }
                 setIsResettingPassword(false);
                 window.alert("Password reset code sent via email");
             }
-            catch(e){
+            catch (e) {
                 window.alert(e.message);
                 setIsResettingPassword(false);
             }
         }
 
-        else if(person == "Staff"){
-            try{
+        else if (person == "Staff") {
+            try {
                 const response = await fetch(`${backEndURL}/staffResetEmail`, {
                     method: "POST",
                     headers: {
@@ -77,25 +117,25 @@ const SignIn = () => {
                     body: JSON.stringify({ staffID: ID, password: "" })
                 });
                 const data = await response.json();
-                if(!data.success){
+                if (!data.success) {
                     throw new Error("User not found");
                 }
                 setIsResettingPassword(false);
                 window.alert("Password reset code sent via email");
             }
-            catch(e){
+            catch (e) {
                 window.alert(e.message);
                 setIsResettingPassword(false);
             }
         }
 
-        else{
+        else {
             setError("Invalid login");
             setIsResettingPassword(false);
         }
     }
 
-    const selectPerson = () =>{
+    const selectPerson = () => {
         console.log(ID);
         if (ID.length == 6) {
             person = 'Staff';
@@ -106,7 +146,7 @@ const SignIn = () => {
         else if (ID[0] == 'D') {
             person = 'Driver';
         }
-        else if(ID[0] == "a"){
+        else if (ID[0] == "a") {
             person = 'Admin';
         }
         else {
@@ -127,7 +167,7 @@ const SignIn = () => {
                         headers: {
                             'Content-Type': 'application/json'
                         },
-                        body: JSON.stringify({ staffID, password, role: "Staff" })
+                        body: JSON.stringify({ staffID, password, role: "Staff", keepLogged: rememberMe })
                     });
                     break;
                 case 'Student':
@@ -137,7 +177,7 @@ const SignIn = () => {
                         headers: {
                             'Content-Type': 'application/json'
                         },
-                        body: JSON.stringify({ studentID, password, role: "Student"})
+                        body: JSON.stringify({ studentID, password, role: "Student", keepLogged: rememberMe })
                     });
                     break;
                 case 'Driver':
@@ -148,7 +188,7 @@ const SignIn = () => {
                         headers: {
                             'Content-Type': 'application/json'
                         },
-                        body: JSON.stringify({ driverID, password, role:"Driver"})
+                        body: JSON.stringify({ driverID, password, role: "Driver", keepLogged: rememberMe })
                     });
                     break;
                 case 'Admin':
@@ -158,22 +198,33 @@ const SignIn = () => {
                         headers: {
                             'Content-Type': 'application/json'
                         },
-                        body: JSON.stringify({ adminID, password, role: "Admin"})
+                        body: JSON.stringify({ adminID, password, role: "Admin", keepLogged: rememberMe })
                     });
                     break;
                 default:
                     setError("Incorrect Login");
                     break;
             }
-            if (response.ok && await response.json()) {
+            const data = await response.json();
+
+            const accessToken = data.accessToken;
+            const role = data.role;
+            const retrievedID = data.id;
+
+            console.log(accessToken, role, retrievedID);
+
+            if (response.ok && accessToken && role && retrievedID) {
                 window.alert("login successfull");
-                if(person == "Student" || person == "Staff"){
-                    //window.location.href = "/home";
+                if (person == "Student" || person == "Staff") {
+                    window.location.href = "/home";
                 }
-                else if(person == "Driver"){
-                    window.location.href= `/shuttleService/${driverID}`
+                else if (person == "Driver") {
+                    window.location.href = `/shuttleService/${driverID}`
                 }
-                else{
+                else if (person == "Admin") {
+                    window.location.href = "/adminDashboard";
+                }
+                else {
                     setError("Error log in");
                 }
             } else {
@@ -182,7 +233,7 @@ const SignIn = () => {
         } catch (error) {
             setError("An error occurred while logging in. Please try again later.");
         }
-        finally{
+        finally {
             setIsloading(false);
         }
 
@@ -259,8 +310,8 @@ const SignIn = () => {
                                 sx={{
                                     color: { xs: '#D4790E', sm: '#D4790E', md: '#D4790E', lg: '#002147FF' }, fontFamily: 'inter', textAlign: 'center',
                                     fontSize: { xs: '16px', sm: '20px', md: '24px', lg: '24px' },
-                                    mt: { xs: 4, sm: 2.5, md: 8, lg: 10 },
-                                    
+                                    mt: { xs: 4, sm: 2.5, md: 8, lg: 8 },
+
                                 }}>
                                 WELCOME TO
                             </Typography>
@@ -269,7 +320,7 @@ const SignIn = () => {
                                 color: '#002147FF', fontFamily: 'inter', textAlign: 'center',
                                 fontSize: { xs: '20px', sm: '22px', md: '30px', lg: '32px' },
                                 mb: { xs: 1, sm: 2, md: 2, lg: 3 },
-                                 fontWeight: '800'
+                                fontWeight: '800'
                             }}>
                                 CINEC SHUTTLE SERVICES
                             </Typography>
@@ -279,7 +330,7 @@ const SignIn = () => {
                                 fontSize: { xs: '12px', sm: '16px', md: '16px', lg: '16px' },
                                 mt: { xs: 2, sm: 3, md: 3, lg: 5 },
                                 mb: { xs: 2, sm: 2, md: 2, lg: 2 },
-                                
+
                             }}>
                                 Enter your ID & Password to SignIn
                             </Typography>
@@ -294,7 +345,7 @@ const SignIn = () => {
                                         mb: { xs: 0.5, sm: 1, md: 1.5, lg: 1.5 },
                                     }, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                                 }} noValidate autoComplete="off">
-                                <TextField id="outlined-basic" label="" variant="outlined" placeholder="ID" onChange={(e) => {setID(e.target.value)}}
+                                <TextField id="outlined-basic" label="" variant="outlined" placeholder="ID" onChange={(e) => { setID(e.target.value) }}
 
                                     InputProps={{ startAdornment: (<InputAdornment position="start" ><AccountCircleOutlinedIcon /></InputAdornment>), }}
                                     sx={{
@@ -318,28 +369,28 @@ const SignIn = () => {
                                         mb: { xs: 0.5, sm: 1, md: 1, lg: 1.5 },
                                     }, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                                 }} noValidate autoComplete="off">
-                                <TextField id="outlined-basic" label="" variant="outlined" type={showPassword ? "text" : "password"} placeholder="Password" 
+                                <TextField id="outlined-basic" label="" variant="outlined" type={showPassword ? "text" : "password"} placeholder="Password"
                                     onChange={(e) => {
                                         setPassword(e.target.value);
                                         setIsTyping(e.target.value.length > 0); // Toggle isTyping based on input
-                                      }}
-                                        InputProps={{
+                                    }}
+                                    InputProps={{
                                         startAdornment: (
                                             <InputAdornment position="start">
-                                            <LockIcon />
+                                                <LockIcon />
                                             </InputAdornment>
                                         ),
                                         endAdornment: isTyping ? ( // Show the visibility toggle only while typing
                                             <InputAdornment position="end">
-                                              <IconButton
-                                                onClick={() => setShowPassword((prev) => !prev)}
-                                                edge="end"
-                                              >
-                                                {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
-                                              </IconButton>
+                                                <IconButton
+                                                    onClick={() => setShowPassword((prev) => !prev)}
+                                                    edge="end"
+                                                >
+                                                    {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                                                </IconButton>
                                             </InputAdornment>
-                                          ) : null,
-                                        }}
+                                        ) : null,
+                                    }}
                                     sx={{
                                         width: { xs: '180px', sm: '300px', md: '320px', lg: '380px' },
                                         '& .MuiOutlinedInput-root': {
@@ -350,31 +401,22 @@ const SignIn = () => {
                                         '& input': { padding: '0 5px', fontSize: '12px', color: '#002147FF', height: '100%', },
                                     }} />
                             </Box>
-
-
                             <Button sx={{
                                 alignItems: 'center', justifyContent: 'center', justifyItems: 'center', display: 'flex', color: '#002147CC', fontFamily: 'inter',
-                                marginLeft: { xs: '160px', sm: '280px', md: '480px', lg: '480px' },
+                                marginLeft: { xs: '140px', sm: '280px', md: '240px', lg: '380px' },
                                 fontSize: { xs: '9px', sm: '12px', md: '15px', lg: '15px' }, fontWeight: 300,
                                 mb: 1,
                                 mt: 0
                             }} onClick={handleResetPassword} disabled={isResettingPassword}>
                                 {isResettingPassword ? 'Resetting Password...' : 'Forgot Password? '}
                             </Button>
-                            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mb: 1 }}>
-    <Checkbox
-        checked={rememberMe} 
-        onChange={(e) => setRememberMe(e.target.checked)} 
-        sx={{ color: '#002147CC',transform: 'scale(0.8)' }} 
-    />
-    <Typography sx={{fontSize: { xs: '10px', sm: '12px', md: '15px', lg: '15px' }, fontWeight: 200, color: '#002147CC' }}>Remember Me</Typography>
-</Box>
-
                             
-                            {error  &&  (
-                            <Typography sx={{ color: "red", fontSize: "12px", marginBottom: "16px", textAlign:'center', marginRight:{xs:'8%',md:'0'}, marginLeft:{xs:'8%', md:'0'}}}>
-                                {error}
-                            </Typography>
+
+
+                            {error && (
+                                <Typography sx={{ color: "red", fontSize: "12px", marginBottom: "16px", textAlign: 'center', marginRight: { xs: '8%', md: '0' }, marginLeft: { xs: '8%', md: '0' } }}>
+                                    {error}
+                                </Typography>
                             )}
 
                             <Box  //Box of Button that used to center the box
@@ -393,22 +435,31 @@ const SignIn = () => {
                                         width: { xs: '170px', sm: '290px', md: '310px', lg: '370px' },
                                         height: { xs: '34px', sm: '40px', md: '45px', lg: '50px' },
                                         borderRadius: '30px',
-                                        mb: 2,
+                                        mb: 0,
                                         '&:hover': {
                                             bgcolor: '#D4790E',
                                         },
-                                    }} 
+                                    }}
                                     onClick={handleSignIn}
                                     disabled={isLoading}
-                                    >
-                                    {isLoading?<CircularProgress size={24} color="inherit"/> : "Sign In"}
+                                >
+                                    {isLoading ? <CircularProgress size={24} color="inherit" /> : "Sign In"}
                                 </Button>
+                            </Box>
+                            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mb: 0 , gap: 0, }}>
+                            <Checkbox
+                                    checked={rememberMe}
+                                    onChange={(e) => setRememberMe(e.target.checked)}
+                                    sx={{ color: '#002147CC', transform: 'scale(0.6)',marginLeft: '-3% '}}	
+                                />
+                                <Typography sx={{ fontSize: { xs: '10px', sm: '12px', md: '15px', lg: '15px' }, fontWeight: 200, color: '#002147CC',marginLeft: '-1% '}}>Remember Me</Typography>
+                               
                             </Box>
 
                             <Typography sx={{
                                 color: '#002147FF', fontFamily: 'inter', textAlign: 'center',
                                 fontSize: { xs: '10px', sm: '14px', md: '14px', lg: '16px' },
-                                mb:{xs:'-1%',md:'0'},  fontWeight: 300,
+                                mb: { xs: '-1%', md: '0' }, fontWeight: 300,
                             }}>
                                 If you don't have account
                             </Typography>
