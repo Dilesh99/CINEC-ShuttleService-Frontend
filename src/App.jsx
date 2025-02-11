96da5986440ab4fefb58b626fd4448f140219d39
Dilesha Gayashan <m20010808008@student.cinec.edu>
2025-02-11 14:26:54 +0530

    booing done

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ function App() {
 
       <BrowserRouter>
         <Routes>
-        <Route index element={<SignIn/>} ></Route>
+        <Route index element={<TransportBookingForm/>} ></Route>
         
         <Route path='/interaction' element={<Interaction/>} ></Route>
         <Route path='/signup' element={<SignUp/>} ></Route>
